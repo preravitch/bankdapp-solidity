4720be5f7930d7cf9d366eec025fb195826f91c3
Preravitch Siripanich <preravitch.s@gmail.com>
2022-07-07 15:57:51 +0700

    edit

--- contracts/Bank.sol
+++ contracts/Bank.sol
@@ -12,7 +12,7 @@ contract Bank {
     constructor() {
     owner = msg.sender;
   }
-    function getAddress(address UsdtAddress) external {
+    function getUsdtAddress(address UsdtAddress) external {
         require(msg.sender == owner, "No Permission");
         Usdt = UsdtAddress;
     }
@@ -27,24 +27,7 @@ contract Bank {
     mapping(address => mapping(string => BankAccont)) public AccountDetails;
     mapping(address => BankAccont) public accountwallet;
 
-    function createnewaccount(string memory _accountname) public {
-        BankAccont memory newAccount = BankAccont (_accountname, 0, msg.sender);
-        Accounts.push(newAccount);
-    }
-
-    function myAccounts() public view returns (string[] memory) {
-        string[] memory myaccount = new string[](Accounts.length);
-        uint count = 0;
-        for (uint i = 0; i < Accounts.length; i++) {
-            if (Accounts[i].owner == msg.sender) {
-                myaccount[count] = Accounts[i].name;
-                count++;
-            }
-        }
-        return myaccount;
-    }
-
-    /*function myAccountsalldetail() public view returns (BankAccont[] memory) {
+    function userAccounts() public view returns (BankAccont[] memory) {
         BankAccont[] memory myaccount = new BankAccont[](Accounts.length);
         uint count = 0;
         for (uint i = 0; i < Accounts.length; i++) {
@@ -54,51 +37,81 @@ contract Bank {
             }
         }
         return myaccount;
-    }*/
+    }
+
+    function userAccountsdetails() public view returns (BankAccont[] memory) {
+        BankAccont[] memory myaccount = new BankAccont[](Accounts.length);
+        uint count = 0;
+        for (uint i = 0; i < Accounts.length; i++) {
+            if (Accounts[i].owner == msg.sender) {
+                myaccount[count] = Accounts[i];
+                count++;
+            }
+        }
+        return myaccount;
+    }
+
+    function createnewaccount(string memory _accountname) public {
+        BankAccont memory newAccount = BankAccont (_accountname, 0, msg.sender);
+        Accounts.push(newAccount);
+    }
+
+    function compareStringsbyBytes(string memory s1, string memory s2) public pure returns(bool){
+    return keccak256(abi.encodePacked(s1)) == keccak256(abi.encodePacked(s2));
+    }
 
     function getBalance(string memory _accountname) public view returns(uint) {
-       return AccountDetails[msg.sender][_accountname].balance;
+       uint balance;
+       for (uint i = 0; i < Accounts.length; i++) {
+            if (compareStringsbyBytes(Accounts[i].name,_accountname)) {
+                balance = Accounts[i].balance;
+            }
+        }
+        return balance;
     }
 
     function depositeUsdt(string memory _accountname, uint _amount) external {
-       AccountDetails[msg.sender][_accountname].balance += _amount;
+       uint index;
+       for (uint i = 0; i < Accounts.length; i++) {
+            if (compareStringsbyBytes(Accounts[i].name,_accountname)) {
+                index = i;
+            }
+        }
+        Accounts[index].balance += _amount;
        IERC20(Usdt).transferFrom(msg.sender, address(this), _amount);
     }
 
     function withdrawUsdt(uint _amount, string memory _accountname) public {
-       require(AccountDetails[msg.sender][_accountname].balance >= _amount, "not enough funds");
+        uint index;
+        for (uint i = 0; i < Accounts.length; i++) {
+            if (compareStringsbyBytes(Accounts[i].name,_accountname)) {
+                index = i;
+            }
+        }
+       require(Accounts[index].balance >= _amount, "not enough funds");
 
-        AccountDetails[msg.sender][_accountname].balance -= _amount;
+        Accounts[index].balance -= _amount;
         IERC20(Usdt).transfer(msg.sender, _amount);
     }
 
-    function withdrawAllUsdt(string memory _accountname) public {
-
-       uint balanceToSend = AccountDetails[msg.sender][_accountname].balance;
-    
-       AccountDetails[msg.sender][_accountname].balance = 0;
-       IERC20(Usdt).transfer(msg.sender, balanceToSend);
-    }
-
     function transferUsdt(string memory _accountfrom, string memory _accountto, uint _amount) public {
-       require(AccountDetails[msg.sender][_accountfrom].balance >= _amount, "not enough funds");
+        uint from;
+        uint to;
+        for (uint i = 0; i < Accounts.length; i++) {
+            if (compareStringsbyBytes(Accounts[i].name, _accountfrom)) {
+                from = i;
+            } else if  (compareStringsbyBytes(Accounts[i].name, _accountto)) {
+                to = i;
+            }
+        }
+        require(Accounts[from].balance >= _amount, "not enough funds");
         
-      AccountDetails[msg.sender][_accountfrom].balance -= _amount;
+        Accounts[from].balance -= _amount;
         //checksameowner
-       if (AccountDetails[msg.sender][_accountfrom].owner != AccountDetails[msg.sender][_accountto].owner) {
+        if (Accounts[from].owner != Accounts[to].owner) {
             _amount = _amount * 99 / 100;
         }
-      AccountDetails[msg.sender][_accountto].balance += _amount;
+        Accounts[to].balance += _amount;
     }
 
-    function transferAllUsdt(string memory _accountfrom, string memory _accountto) public {
-
-        uint balanceToSend = AccountDetails[msg.sender][_accountfrom].balance;
-       AccountDetails[msg.sender][_accountfrom].balance = 0;
-        //checksameowner
-       if (AccountDetails[msg.sender][_accountfrom].owner != AccountDetails[msg.sender][_accountto].owner) {
-            balanceToSend = balanceToSend * 99 / 100;
-        }
-       AccountDetails[msg.sender][_accountto].balance = balanceToSend;
-    }
 }
